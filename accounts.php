dead9b3a84cc9837a29700d1be37752f81f3e913
NhutLinh-99 <43626765+NhutLinh-99@users.noreply.github.com>
2018-10-30 22:27:45 +0700

    delete file cũ

--- accounts.php
+++
@@ -1,9 +0,0 @@
-<?php
-include './db.php';
-class accounts extends db {
-    public function  selectuser(){
-        $sql = "SELECT * FROM accounts";
-    $result = $this->query($sql);
-    return $result;
-    }
-}
